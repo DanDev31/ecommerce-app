19e24b44f9fd977acfc724d01cf040948bc0d4b1
Daniel <juandaniel.25@outlook.com>
2022-06-23 17:12:11 -0500

    Searchbar

--- client/src/components/navbar/search_bar/SearchBar.jsx
+++ client/src/components/navbar/search_bar/SearchBar.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
+import { useState } from 'react'
 import { GoSearch } from 'react-icons/go'
 import styles from './search_bar.module.scss'
 
 export const SearchBar = () => {
 
-  
+  const [ search, setSearch ] = useState('')
 
+  const handleChange = ({ target }) => {
+    setSearch(target.value)
+  }
+
+  console.log(search)
   return (
     <div className={ styles.search_bar_container }>
         <form className={ styles.search_bar_form }>
-            <input type="text" placeholder="Search products..."/>
+            <input type="text" value={ search } placeholder="Search products..." onChange={ handleChange }/>
             <button type='submit'><GoSearch/></button>
         </form>
     </div>
