08bdf7d849f21354a1bdfca31a59e6efa2ddaa0c
Daniel <juandaniel.25@outlook.com>
2022-06-27 23:50:33 -0500

    Filtros y sort terminados

--- client/src/components/filters/sort/Sort.jsx
+++ client/src/components/filters/sort/Sort.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
+import { useDispatch } from 'react-redux'
+import { produtsByCategoryActions } from '../../../redux/products/productsByCategorySlice'
 
 export const Sort = () => {
+
+  const dispatch = useDispatch()
+  
+  const handleSelectChange = ({target}) => {
+    dispatch(produtsByCategoryActions.orderByPrice(target.value))
+  }
+
   return (
     <div>
-        <select name="" id="">
+        <select name="" id="" onChange={ handleSelectChange }>
             <option value="">Order by</option>
-            <option value="">Most Popular</option>
-            <option value="">Higher price</option>
-            <option value="">Lower Price</option>
+            <option value="high">Higher price</option>
+            <option value="low">Lower Price</option>
         </select>
     </div>
   )
