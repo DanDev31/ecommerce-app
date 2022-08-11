3ec3ad658ec042920fa4981ecdb54c54c2d4e364
Daniel <juandaniel.25@outlook.com>
2022-08-10 20:58:10 -0500

    Version 1.0

--- client/src/components/order/Order.jsx
+++ client/src/components/order/Order.jsx
@@ -1,58 +1,15 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
-import StripeCheckout from 'react-stripe-checkout';
-import axios from 'axios';
+import { useSelector } from 'react-redux';
 
-const stripe_key=process.env.REACT_APP_STRIPE_KEY
+
 
 export const Order = () => {
-
-    const [ stripeToken, setStripeToken ] = useState(null)
-   
-    const onToken = (token) => {
-        setStripeToken(token)
-    }
-
-    useEffect(() => {
-        const makeStripeRequest = async() => {
-            try {
-                const res = await axios.post('/order/payment', {
-                        tokenId:stripeToken.id,
-                        amount:2000
-                    })
-                    console.log(res.data)
-            }
-            catch (error) {
-                    console.log(error)
-            }
-        }
-
-        stripeToken && makeStripeRequest()
-    }, [stripeToken])
-
-  return (
-    <div>
-        Order
-
-        {
-            stripeToken ? (<span>Processing your order. Please wait...</span>)
-            :
-            (
-                <StripeCheckout
-                        name='TechnoShop'
-                        image='https://services.tochat.be/icns/categories/e-commerce.png'
-                        billingAddress
-                        shippingAddress
-                        description='hola'
-                        amount={2000}
-                        token={onToken}
-                        stripeKey={stripe_key}
-                    >
-                        <button>Pay now</button>
-                </StripeCheckout>
-            )
-        }
-        
-    </div>
-  )
+    const { user } = useSelector(state => state.user)
+    return (
+        <div>
+            <h2>Hi, {user?.name}</h2>
+            <p>Your Orders:</p>
+        </div>
+    )
 }
