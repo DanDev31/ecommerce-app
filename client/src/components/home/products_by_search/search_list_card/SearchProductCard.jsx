619cbc7bcb5327220e185fc37d5002c856b65919
Daniel <juandaniel.25@outlook.com>
2022-08-12 16:47:14 -0500

    app updated

--- client/src/components/home/products_by_search/search_list_card/SearchProductCard.jsx
+++ client/src/components/home/products_by_search/search_list_card/SearchProductCard.jsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { ProductStyles } from "../../../styles/Products";
-
+import { CircularProgress } from '@mui/material'; 
 
 
 export const SearchProductCard = ({
@@ -13,23 +13,40 @@ export const SearchProductCard = ({
   categoryId,
 }) => {
 
+  const [ loading, setLoading ] = useState(true)
   const { categories } = useSelector((state) => state.categories);
   const category = categories.find((e) => e.id === categoryId);
+
+  useEffect(() => {
+    if(category){
+      setLoading(false)
+    }
+    
+  },[loading])
 
   
   return (
     <ProductStyles>
-      <Link to={`/shop/${category ? category.category_name : null}/${id}`} className="anchor">
-          <div className="product_container">
-            <div className="product_img_container">
-              <img src={product_image} alt="" />
-            </div>
-          <div>
-            <h4>{product_name}</h4>
-            <p>${price}</p>
-          </div>
-          </div>
-      </Link>
+      {
+        loading ? 
+        (
+          <CircularProgress />
+        )
+        :
+        (
+          <Link to={`/shop/${category ? category.category_name : null}/${id}`} className="anchor">
+              <div className="product_container">
+                <div className="product_img_container">
+                  <img src={product_image} alt="" />
+                </div>
+              <div>
+                <h4>{product_name}</h4>
+                <p>${price}</p>
+              </div>
+              </div>
+          </Link>
+        )
+      }
     </ProductStyles>
   );
 };
